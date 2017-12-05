b6d951760099eae92037d941f2ec1fa9fb73e358
Mark Wu <mang.wu@gmail.com>
2017-12-04 22:49:41 -0500

    simplify SpringBootApplication class, remove webinit

--- my-kotlin/src/main/java/hello/MyApplication.kt
+++ my-kotlin/src/main/java/hello/MyApplication.kt
@@ -5,9 +5,7 @@ import org.glassfish.jersey.servlet.ServletContainer
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.web.servlet.ServletRegistrationBean
-import org.springframework.boot.web.support.SpringBootServletInitializer
 import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
@@ -16,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseBody
-import java.util.Date
 import javax.servlet.http.HttpServlet
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
@@ -30,8 +27,7 @@ fun main(args: Array<String>) {
 }
 
 @SpringBootApplication
-open class MyApplication: SpringBootServletInitializer() {
-    override fun configure(builder: SpringApplicationBuilder) = builder.sources(MyApplication::class.java)
+open class MyApplication {
     @Bean open fun messageBean() = "Hello message"
     @Bean open fun helloServletRegistrationBean() = ServletRegistrationBean(HelloServlet(), "/hello-servlet")
     @Bean open fun jerseyRegistrationBean(): ServletRegistrationBean {
